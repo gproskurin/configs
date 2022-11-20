597b30601c879db1b315f46a4e11d98e34e2bdc7
Gennady Proskurin <gpr@mail.ru>
2022-11-20 13:32:29 +0000

    xmonad: update config for newer xmonad

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -5,11 +5,18 @@ import XMonad.Layout.Fullscreen
 import XMonad.Layout.NoBorders
 import XMonad.Layout.Tabbed
 import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeys)
-import XMonad.Util.EZConfig(additionalKeysP)
+--
+--import XMonad.Util.EZConfig(additionalKeys)
+--import XMonad.Util.EZConfig(additionalKeysP)
+import XMonad.Util.EZConfig
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.SetWMName
+
 import System.IO
 
-myTabConfig = defaultTheme
+
+-- myTabConfig = defaultTheme
+myTabConfig = def
     { activeBorderColor = "#FF0000"
     , activeTextColor = "#FFFFFF"
     , inactiveBorderColor = "#000000"
@@ -24,16 +31,20 @@ myLayout =
     )
     ||| noBorders (fullscreenFull Full)
 
-myEventHook = mconcat [ docksEventHook, handleEventHook defaultConfig ]
+-- myEventHook = mconcat [ docksEventHook, handleEventHook defaultConfig ]
+myEventHook = handleEventHook def
 
 main = do
     xmproc <- spawnPipe "xmobar"
-    xmonad $ defaultConfig
-        { modMask = mod4Mask
+    -- xmonad $ defaultConfig
+    xmonad $ def
+        { startupHook = setWMName "LG3D"
+        , modMask = mod4Mask
         , borderWidth = 2
         , terminal = "urxvt"
-        , manageHook = manageDocks <+> manageHook defaultConfig
-        --, layoutHook = avoidStruts  $  layoutHook defaultConfig
+        -- , manageHook = manageDocks <+> manageHook defaultConfig
+        , manageHook = manageDocks <+> manageHook def
+        -- , layoutHook = avoidStruts  $  layoutHook defaultConfig
         , layoutHook = myLayout
 	, handleEventHook = myEventHook
         , logHook = dynamicLogWithPP xmobarPP
